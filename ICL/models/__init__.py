d3d8e0fe34073e75fa9002c6f2201baa59804432
Hector Manuel Lopez Rios <hectorlopez3100@gmail.com>
2025-11-18 10:40:44 -0600

    added WTA network to test for ICL

--- ICL/models/__init__.py
+++ ICL/models/__init__.py
@@ -7,10 +7,12 @@ This package contains different model architectures for in-context learning.
 from .base_icl_model import BaseICLModel
 from .markov_icl import MatrixTreeMarkovICL
 from .polynomial_icl import RandomPolynomialICL
+from .wta_icl import WinnerTakesAllICL
 
 __all__ = [
     'BaseICLModel',
     'MatrixTreeMarkovICL',
-    'RandomPolynomialICL'
+    'RandomPolynomialICL',
+    'WinnerTakesAllICL'
 ]
 
